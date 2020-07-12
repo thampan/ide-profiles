8e11f777bdd08542e95266bfacdf89cc9ea4cf73
thampan <jishnu.mt@gmail.com>
2020-07-12 23:03:53 +0200

    more plugin

--- setup.bash
+++ setup.bash
@@ -4,16 +4,34 @@ set -eux
 mkdir -p ~/.vim/bundle/
 mkdir -p ~/.vim/autoload/
 
-wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -P ~/.vim/autoload/
+plugins=(
+https://github.com/Raimondi/delimitMate.git 
+https://github.com/yegappan/mru.git
+https://github.com/preservim/nerdtree.git
+git://github.com/altercation/vim-colors-solarized.git
+)
+
+if [ ! -f  /home/$USER/.vim/autoload/ ]; then
+	wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -P ~/.vim/autoload/
+fi
+
 echo "execute pathogen#infect()" > ~/.vimrc
 echo "filetype plugin indent on" >> ~/.vimrc
 echo "syntax on" >> ~/.vimrc
 echo "set ic" >> ~/.vimrc
+#echo "set background=dark" >> ~/.vimrc
+#echo "colorscheme	solarized" >> ~/.vimrc
 echo "set shiftwidth=2" >> ~/.vimrc
 echo "set tabstop=2" >> ~/.vimrc
 echo "set bg=dark" >> ~/.vimrc
 
-cd  ~/.vim/bundle
-if [ ! -d  delimitMate ]; then
-	git clone https://github.com/Raimondi/delimitMate.git 
-fi
+pushd  ~/.vim/bundle
+for f in ${plugins[@]};
+do
+	folder=${f##*/}
+	if [ ! -d  ${folder%.*} ]; then
+		git clone $f
+	fi
+done
+popd
+
