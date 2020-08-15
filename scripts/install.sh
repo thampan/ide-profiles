77747be17f585fab97eba1fa03c5ff05f5877156
thampan <jishnu.mt@gmail.com>
2020-08-15 10:26:43 +0200

    intendation corrected

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,25 +9,25 @@ mapfile  -t plugins < $FILES/plugins.conf
 mapfile  -t vimrc   < $FILES/vimrc_settings.conf
 
 if [ ! -f  /home/$USER/.vim/autoload/ ]; then
-	wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -P ~/.vim/autoload/
+   wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -P ~/.vim/autoload/
 fi
 
 for settings in "${vimrc[@]}";
 do
-	echo $settings >> ~/vimrc
+  echo $settings >> ~/vimrc
 done
 
 pushd  ~/.vim/bundle
 for f in ${plugins[@]};
 do
-	if [[ $f == "#"* ]];
-	then
-		continue;
-	fi
-	folder=${f##*/}
-	if [ ! -d  ${folder%.*} ]; then
-		git clone https://github.com/$f
-	fi
+  if [[ $f == "#"* ]];
+  then
+    continue;
+  fi
+  folder=${f##*/}
+  if [ ! -d  ${folder%.*} ]; then
+	  git clone https://github.com/$f
+  fi
 done
 popd
 
