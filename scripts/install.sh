ef072c96fc862f532c9095b449199c3ddb4049b0
thampan <jishnu.mt@gmail.com>
2020-09-01 11:03:56 +0200

    fixed: install bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -eu
 
 DIRNAME=$(dirname {$(basename $0)})
 FILES=$DIRNAME/files
@@ -9,15 +9,25 @@ mapfile  -t plugins < $FILES/plugins.conf
 mapfile  -t vimrc   < $FILES/vimrc_settings.conf
 
 if [ ! -f  /home/$USER/.vim/autoload/ ]; then
-   wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -P ~/.vim/autoload/
+   wget https://github.com/tpope/vim-pathogen/tree/master/autoload/pathogen.vim -P ~/.vim/autoload/
 fi
 
+# Flush exisiting configurations
+echo > ~/.vimrc 
+
+# Pump the new configuration as specified in `files/vimrc_settings.conf`
 for settings in "${vimrc[@]}";
 do
-  echo $settings >> ~/vimrc
+  if [[ $settings == "#"* ]];
+  then
+    continue;
+  fi
+  echo $settings >> ~/.vimrc
 done
 
 pushd  ~/.vim/bundle
+
+# Pump the new configuration as specified in `files/plugins.conf`
 for f in ${plugins[@]};
 do
   if [[ $f == "#"* ]];
